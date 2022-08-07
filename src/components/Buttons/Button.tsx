072d00c3b65b06d732a1560b01a365574d9e1abc
Willyan Lauriente <willyanf17@hotmail.com>
2022-08-07 18:59:20 -0300

    added interface props

--- src/components/Buttons/Button.tsx
+++ src/components/Buttons/Button.tsx
@@ -1,6 +1,4 @@
-type Props = {
-    children: React.ReactNode;
-}
+import { Props } from 'interfaces';
 
 export function Button({ children }: Props) {
     return <button className="py-2 px-4 rounded-md bg-green-500 text-white font-bold w-44 hover:bg-green-600 transition-all">{children}</button>
